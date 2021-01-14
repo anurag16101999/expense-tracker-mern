5f2d6d958ac9c08a68dfe75e06296e63af4bc89f
Anurag Gupta <guptaanurag1610@gmail.com>
2021-01-14 16:23:46 +0530

    Added database and controllers

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useCreayeIndex: true,
+      useUnifiedTopology: true,
+    });
+    console.log(
+      `MongoDB connected:${conn.connection.host} `.cyan.underline.bold
+    );
+  } catch (err) {
+    console.log(`error: ${err.message}`.red);
+    precess.exit(1);
+  }
+};
+
+module.exports = connectDB;
