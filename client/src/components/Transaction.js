244d13b62fde9f3641790e40379a2881ac8e2b15
Anurag Gupta <guptaanurag1610@gmail.com>
2021-01-14 19:25:35 +0530

    added all functionalities

--- client/src/components/Transaction.js
+++ client/src/components/Transaction.js
@@ -10,7 +10,7 @@ export const Transaction = ({ transaction }) => {
         {sign}Rs.{Math.abs(transaction.amount)}
       </span>
       <button
-        onClick={() => deleteTransaction(transaction.id)}
+        onClick={() => deleteTransaction(transaction._id)}
         className="delete-btn"
       >
         x
